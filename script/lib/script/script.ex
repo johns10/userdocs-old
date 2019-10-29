d918976efb7af4aacd9f71efa6d4c4417160f227
johns10 <johns10@gmail.com>
2019-10-28 23:08:43 -0400

    modified:   script/lib/script/script.ex 	modified:   script/test/script_test.exs

--- script/lib/script/script.ex
+++ script/lib/script/script.ex
@@ -1,54 +1,65 @@
 defmodule Script.Script do
+  @doc """
+    Takes a script, a list of types, a list of named components with prototypes
+    and arguments, generates script components from each entry and concatenates
+    them.
+    script: the script to concatenate additional entries to.  Initially should
+    be empty
+    list of script maps, each with a type and a list of args
+    types: a list of types with a type, prototype, and list of valid parameters
 
-  defstruct(
-    script_types: [
-      %{
-        :type   => :outline,
-        :params => [ :color, :thickness ],
-        :script =>
-        '''
-        element.style.outline = '{0} solid {1}px';\n
-        '''
-      }, %{
-        :type   => :badge,
-        :params => [ :radius, :label ],
-        :script =>
-        '''
-        var size = {0};
-        var label = {1};
-        var color = '{2}';
+    Returns the interpolated string
+    """
+    @doc since: "1.9.2"
+    #TODO: Re-implement as a prototype
+  def generate_script(script, [ %{ type: type, args: args} | list ], types) do
+    generate_prototype(type, types[type].prototype, args)
+    |> interpolate_prototype()
+    |> generate_script(list, types)
+    |> prepend(script)
+  end
+  def generate_script(script, [], _) do
+    script
+  end
 
-        var wrapper = document.createElement('div');
-        var badge = document.createElement('span');
-        var textnode = document.createTextNode(label);
+  @doc """
+    Generates a named script component, with some arguments.  Mostly for type checking.
+    prototype: an eex string with named attributes
+    args: a list of named arguments
 
+    Returns the interpolated string
+    """
+    @doc since: "1.9.2"
+    #TODO: Re-implement as a prototype
+  def generate_prototype(:select_xpath, prototype, args = [ selector: _selector ]) do
+    { prototype, args }
+  end
+  def generate_prototype(:outline, prototype, args = [ color: _color, thickness: _thickness ]) do
+    { prototype, args }
+  end
+  def generate_prototype(:badge, prototype, args = [ radius: _radius, label: _label, color: _color ]) do
+    { prototype, args }
+  end
+  def generate_prototype(_, prototype, args) do
+    { prototype, args }
+  end
+  @doc """
+    Interpolates an eex string with named attributes.
+    prototype: an eex string with named attributes
+    args: a list of named arguments
 
-        element.appendChild(wrapper);
-        badge.appendChild(textnode);
-        wrapper.appendChild(badge);
+    Returns the interpolated string
+    """
+  @doc since: "1.9.2"
+  def interpolate_prototype({ prototype, args }) do
+    EEx.eval_string(prototype, args)
+  end
 
-        element.style.position = 'relative';
-
-        wrapper.style.display = '';
-        wrapper.style.justifyContent = 'center';
-        wrapper.style.alignItems = 'center';
-        wrapper.style.minHeight = '';
-
-        wrapper.style.position = 'absolute';
-        wrapper.style.top = (-1 * size).toString(10) + 'px';
-        wrapper.style.right = (-1 * size).toString(10) + 'px';
-
-        badge.style.display = 'inline-block';
-        badge.style.minWidth = '16px';
-        badge.style.padding = (0.5 * size).toString(10) + 'px ' + (0.5 * size).toString(10) + 'px';
-        badge.style.borderRadius = '50%';
-        badge.style.fontSize = '25px';
-        badge.style.textAlign = 'center';
-        badge.style.background = color;
-        badge.style.color = 'white';
-        '''
-      }
-    ]
-  )
-
+  def prepend(tail, []) do
+    tail
+  end
+  def prepend(tail, head) do
+    head <> tail
+  end
 end
+
--- script/test/script_test.exs
+++ script/test/script_test.exs
@@ -2,7 +2,64 @@ defmodule ScriptTest do
   use ExUnit.Case
   doctest Script
 
-  test "greets the world" do
-    assert Script.hello() == :world
+  test "generates a whole script" do
+    types = %{
+      select_xpath: %{
+        prototype: "Selector: <%= selector %>\n",
+        args: [ 'selector' ]
+      },
+      outline: %{
+        prototype: "Color: <%= color %>, Thickness: <%= thickness %>\n",
+        args: [ 'color', 'thickness' ]
+      },
+      badge: %{
+        prototype: "Radius: <%= radius %>, Label: <%= label %>, Color: <%= color %>\n",
+        args: [ 'radius', 'label', 'red' ]
+      }
+    }
+    list = [
+      %{
+        type: :select_xpath,
+        args: [ selector: ~s|/html/body/div[@class='ember-view']/div[9]/div//form//input[@placeholder='Password']|]
+      },
+      %{
+        type: :outline,
+        args: [ color: 'red', thickness: '10' ]
+      },
+      %{
+        type: :badge,
+        args: [ radius: '50', label: '1', color: 'red' ]
+      }
+    ]
+    result = Script.Script.generate_script('', list, types)
+    assert(result == "Selector: /html/body/div[@class='ember-view']/div[9]/div//form//input[@placeholder='Password']\nColor: red, Thickness: 10\nRadius: 50, Label: 1, Color: red\n")
+  end
+
+  test "interpolate string returns matching string" do
+    prototype = "The <%= adj %> <%= adj2 %> <%= noun %>"
+    args = [ adj: "quick", adj2: "brown", noun: "fox" ]
+    result = Script.Script.interpolate_prototype({ prototype, args })
+    assert(result == "The quick brown fox")
+  end
+
+  test "Generates a badge script" do
+    prototype = "Radius: <%= radius %>, Label: <%= label %>, Color: <%= color %>"
+    args = [ radius: '10', label: 'label', color: 'red']
+    result = Script.Script.generate_prototype(:badge, prototype, args)
+    assert(result == { prototype, args })
+  end
+
+  test "Generates an outline script" do
+    prototype = "Color: <%= color %>, Thickness: <%= thickness %>"
+    args = [ color: 'red', thickness: '10' ]
+    result = Script.Script.generate_prototype(:outline, prototype, args)
+    assert(result == { prototype, args })
+  end
+
+  test "Generates an xpath selector script" do
+    prototype = "Selector: <%= selector %>"
+    args = [ selector: 'select' ]
+    result = Script.Script.generate_prototype(:select_xpath, prototype, args)
+    assert(result == { prototype, args })
   end
 end
